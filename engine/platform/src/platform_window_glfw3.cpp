d3bc5d5383db50f6f4706a0d786b28a60e55a2a7
Jhonny Göransson <jhonny.goransson@gmail.com>
2024-10-12 11:11:40 +0200

    Dont use forward compatability for opengl on windows

--- engine/platform/src/platform_window_glfw3.cpp
+++ engine/platform/src/platform_window_glfw3.cpp
@@ -174,11 +174,14 @@ namespace dmPlatform
 
     static PlatformResult OpenWindowOpenGL(Window* wnd, const WindowParams& params)
     {
+    #ifdef __MACH__
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
         glfwWindowHint(GLFW_SAMPLES, params.m_Samples);
 
         GLFWmonitor* fullscreen_monitor = NULL;
